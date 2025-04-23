aea0d17493cab9d0e1fbdd5d4bcf8bacc23e0c95
chuangtc <chuangtcee@gmail.com>
2025-04-24 05:14:57 +0800

    remove GA4 tracking

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -5,6 +5,8 @@ import Script from 'next/script';
 export default function GoogleAnalytics() {
   return (
     <>
+      {/* GA4 scripts are disabled to prevent data collection */}
+      {/* 
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-6KD7M5EKZL"
         strategy="afterInteractive"
@@ -14,9 +16,20 @@ export default function GoogleAnalytics() {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-6KD7M5EKZL');
+          
+          // Disable data collection by setting consent mode to denied
+          gtag('consent', 'default', {
+            'analytics_storage': 'denied',
+            'ad_storage': 'denied'
+          });
+          
+          gtag('config', 'G-6KD7M5EKZL', {
+            'anonymize_ip': true,
+            'restricted_data_processing': true
+          });
         `}
       </Script>
+      */}
     </>
   );
 } 
